efd8b0e4a56cf5394e19c0c4c4ee5dd31fd19017
Adrian <klimasa98@gmail.com>
2023-10-25 21:32:10 -0400

    add function to let player choose which figure he wants to play with

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
+const playerChoose = document.querySelectorAll('.userChoose');
+const gameboardSquares = document.querySelectorAll('.container .box');
 
+console.log(gameboardSquares);
+
+let currentPlayerPick;
+
+const setPlayerChoosedFigure = currPick => {
+	if (currPick.classList.value === 'circle' || currPick.classList.contains('figure-Circle')) {
+		gameboardSquares.forEach(el => {
+			el.lastElementChild.classList.add('circle');
+		});
+	}else{
+        gameboardSquares.forEach(el => {
+			el.lastElementChild.classList.add('cross');
+		});
+    }
+};
+
+playerChoose.forEach(el =>
+	el.addEventListener('click', e => {
+        gameboardSquares.forEach(el => {
+            el.lastElementChild.classList.remove('circle')
+            el.lastElementChild.classList.remove('cross')
+        })
+		currentPlayerPick = e.target;
+		setPlayerChoosedFigure(currentPlayerPick);
+	})
+);
