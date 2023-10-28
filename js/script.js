57e7babd482fc8535df14ed724e63490e63fe2d9
Adrian <klimasa98@gmail.com>
2023-10-28 16:40:05 -0400

    created logic for winning combs

--- js/script.js
+++ js/script.js
@@ -7,32 +7,144 @@ let AIPicked;
 let currentPlayerPick;
 
 const checkWin = () => {
-    allCell.forEach(el => console.log(el.lastElementChild.classList))
-
-
-}
+	if (
+		allCell[0].lastElementChild.classList.contains('cross') &&
+		allCell[1].lastElementChild.classList.contains('cross') &&
+		allCell[2].lastElementChild.classList.contains('cross') 
+	) {
+		console.log('xwin');
+	}
+	if (
+		allCell[3].lastElementChild.classList.contains('cross') &&
+		allCell[4].lastElementChild.classList.contains('cross') &&
+		allCell[5].lastElementChild.classList.contains('cross')
+	) {
+		console.log('xwin');
+	}
+	if (
+		allCell[6].lastElementChild.classList.contains('cross') &&
+		allCell[7].lastElementChild.classList.contains('cross') &&
+		allCell[8].lastElementChild.classList.contains('cross')
+	) {
+		console.log('xwin');
+	}
+	if (
+		allCell[1].lastElementChild.classList.contains('circle') &&
+		allCell[2].lastElementChild.classList.contains('circle') &&
+		allCell[0].lastElementChild.classList.contains('circle')
+	) {
+		console.log('o wins');
+	}
+	if (
+		allCell[3].lastElementChild.classList.contains('circle') &&
+		allCell[4].lastElementChild.classList.contains('circle') &&
+		allCell[5].lastElementChild.classList.contains('circle')
+	) {
+		console.log('o wins');
+	}
+	if (
+		allCell[6].lastElementChild.classList.contains('circle') &&
+		allCell[7].lastElementChild.classList.contains('circle') &&
+		allCell[8].lastElementChild.classList.contains('circle')
+	) {
+		console.log('o wins');
+	}
+	if (
+		allCell[0].lastElementChild.classList.contains('cross') &&
+		allCell[3].lastElementChild.classList.contains('cross') &&
+		allCell[6].lastElementChild.classList.contains('cross')
+	) {
+		console.log('xwin');
+	}
+	if (
+		allCell[1].lastElementChild.classList.contains('cross') &&
+		allCell[4].lastElementChild.classList.contains('cross') &&
+		allCell[7].lastElementChild.classList.contains('cross')
+	) {
+		console.log('xwin');
+	}
+	if (
+		allCell[2].lastElementChild.classList.contains('cross') &&
+		allCell[5].lastElementChild.classList.contains('cross') &&
+		allCell[8].lastElementChild.classList.contains('cross')
+	) {
+		console.log('xwin');
+	}
+	if (
+		allCell[0].lastElementChild.classList.contains('circle') &&
+		allCell[3].lastElementChild.classList.contains('circle') &&
+		allCell[6].lastElementChild.classList.contains('circle')
+	) {
+		console.log('o wins');
+	}
+	if (
+		allCell[1].lastElementChild.classList.contains('circle') &&
+		allCell[4].lastElementChild.classList.contains('circle') &&
+		allCell[7].lastElementChild.classList.contains('circle')
+	) {
+		console.log('o wins');
+	}
+	if (
+		allCell[2].lastElementChild.classList.contains('circle') &&
+		allCell[5].lastElementChild.classList.contains('circle') &&
+		allCell[8].lastElementChild.classList.contains('circle')
+	) {
+		console.log('o wins');
+	}
+	if (
+		allCell[0].lastElementChild.classList.contains('cross') &&
+		allCell[4].lastElementChild.classList.contains('cross') &&
+		allCell[8].lastElementChild.classList.contains('cross')
+	) {
+		console.log('x wins');
+	}
+	if (
+		allCell[2].lastElementChild.classList.contains('cross') &&
+		allCell[4].lastElementChild.classList.contains('cross') &&
+		allCell[6].lastElementChild.classList.contains('cross')
+	) {
+		console.log('x wins');
+	}
+	if (
+		allCell[0].lastElementChild.classList.contains('circle') &&
+		allCell[4].lastElementChild.classList.contains('circle') &&
+		allCell[8].lastElementChild.classList.contains('circle')
+	) {
+		console.log('o wins');
+	}
+	if (
+		allCell[2].lastElementChild.classList.contains('circle') &&
+		allCell[4].lastElementChild.classList.contains('circle') &&
+		allCell[6].lastElementChild.classList.contains('circle')
+	) {
+		console.log('o wins');
+	}
+};
 
 const AIturn = () => {
+    
 	const gameboardSquares = document.querySelectorAll('[data-index]');
 	AIPicked = gameboardSquares[Math.floor(Math.random() * (gameboardSquares.length - 1))].lastElementChild;
 
 	setTimeout(() => {
 		AIPicked.style.opacity = '1';
-		currentPlayerPick === 'cross' ? AIPicked.classList.add('circle') : AIPicked.classList.add('cross');
 	}, 500);
+    currentPlayerPick === 'cross' ? AIPicked.classList.add('circle') : AIPicked.classList.add('cross');
 
 	AIPicked.parentElement.setAttribute('disabled', 'true');
 	AIPicked.parentElement.removeAttribute('data-index');
 
-	checkWin();
-
+	
+    
 	playerTurn();
 };
 
 const playerTurn = () => {
+    checkWin()
 	const gameboardSquares = document.querySelectorAll('[data-index]');
 
 	gameboardSquares.forEach(el => {
+        checkWin()
 		el.addEventListener('click', handlePlayerSquareClick);
 	});
 };
@@ -49,7 +161,7 @@ const handlePlayerSquareClick = event => {
 	el.setAttribute('disabled', 'true');
 	el.removeAttribute('data-index');
 
-	checkWin();
+	
 
 	AIturn();
 };
@@ -71,3 +183,4 @@ playerChoose.forEach(el => {
 		startGame();
 	});
 });
+
